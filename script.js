5aee4240b461bcf3200e2c29e1aa3e4ba329a66a
mementoMori-FCDK <gbezsh@gmail.com>
2021-07-26 20:35:09 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function displayTime() {
+    let date = new Date();
+    let time = date.toLocaleTimeString();
+    let p = document.querySelector(".clock").textContent = time;
+}
+
+displayTime();
+const createClock = setInterval(displayTime, 1000);
